9fa318698762f7109ed87e13612e3a9d74b8c3cf
Sami Koponen <sami.t.koponen@icloud.com>
2026-02-14 12:36:47 +0200

    Login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, login_required, current_user
 from config import Config
 from app.tmdb import fetch_popular_movies
 import random as rand
+
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +16,15 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        from app.models import User
+        return User.query.get(int(user_id))
+
 
     @app.route('/')
     def index():
@@ -26,6 +38,7 @@ def create_app():
         return render_template('random.html', movie=random_movie)
 
     @app.route('/profile')
+    @login_required
     def profile():
 
         return render_template('profile.html')
